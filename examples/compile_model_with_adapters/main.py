ad96d944892a8ab072b06fe96b3fa4052b6c496c
ltoniazzi <lt6ga@protonmail.com>
2024-05-29 14:38:17 +0100

    Add example

--- examples/compile_model_with_adapters/main.py
+++ examples/compile_model_with_adapters/main.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import onnxruntime as ort
 import pathlib
+import netron
 
-ROOT_FOLDER = pathlib.Path(__file__).parent.parent
+ROOT_FOLDER = pathlib.Path(__file__).parent.parent.parent
 
 # Define the main model with conditional LoRA adapters
 class ConditionalLoRAModel(nn.Module):
@@ -45,16 +46,23 @@ class LoRAAdapter(nn.Module):
 
     def forward(self, x):
         return self.lora(x)
-    
 
-def export_model_with_adapters(model, dummy_input):
+if __name__ == "__main__":
+
+    models_folder = ROOT_FOLDER / "models"
+    model_path = str(models_folder / "conditional_lora_model.onnx")
+    use_netron = False
+
+    # Get torch model
+    model = ConditionalLoRAModel()
+
     # Trun it into a graph
     scripted_model = torch.jit.script(model)
 
-    # Dummy input for the adapter selection
-    dummy_adapter_number = torch.tensor([1], dtype=torch.int)
-
     # Export model to onnx format
+    dummy_input = torch.randn(1, 10)
+    dummy_adapter_number = torch.tensor([1], dtype=torch.int)  # Dummy input for the adapter selection
+
     torch.onnx.export(
         scripted_model, 
         (dummy_input, dummy_adapter_number), 
@@ -69,20 +77,22 @@ def export_model_with_adapters(model, dummy_input):
     )
 
 
-if __name__ == "__main__":
-
-    models_folder = ROOT_FOLDER / "models"
-    model_path = str(models_folder / "conditional_lora_model.onnx")
-
-    export_model_with_adapters(model=ConditionalLoRAModel(), dummy_input=torch.randn(1, 10))
-
     # Prepare inputs where int inputs that allow to swap the adapter
     dummy_input = torch.randn(1, 10)
     adapter_number_1 = torch.tensor([1], dtype=torch.int)
     adapter_number_2 = torch.tensor([2], dtype=torch.int)
 
+
+    # Run hot model with two different adapters
     # Load onnx model with python runtime
     ort_session = ort.InferenceSession(model_path)
+
+    # Print input and output names
+    input_name = ort_session.get_inputs()[0].name
+    adapter_name = ort_session.get_inputs()[1].name
+    output_name = ort_session.get_outputs()[0].name
+    print(f"Input Name: {input_name}, Adapter Name: {adapter_name}, Output Name: {output_name}")
+
 
     # Collect inputs
     inputs_list = [
@@ -91,9 +101,21 @@ if __name__ == "__main__":
     ]
 
     for inputs in inputs_list:
+        # Enable profiling
+        sess_options = ort.SessionOptions()
+        sess_options.enable_profiling = True    
+
         # Run inference
         outputs = ort_session.run(None, inputs)
 
         # Retrieve the output and check the output shape 
         print(f"\nModel output with adapter {inputs['adapter_number']}:", outputs[0])
     
+        # Retrieve and print profiling information
+        profile_file = ort_session.end_profiling()
+        print(f"Profile data saved to {profile_file}")
+
+    if use_netron:
+        # Assuming the ONNX file is named "conditional_lora_model.onnx"
+        netron.start(model_path)
+
